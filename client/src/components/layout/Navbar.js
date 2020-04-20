abe8cc3aeb6b643c2171e7f9b2c176345f6c5eee
Abhishek jaswal <abhishek.jaswal.1999@gmail.com>
2020-04-20 21:27:19 +0530

    DevConnect commit

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -44,7 +44,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className='navbar bg-dark'>
       <h1>
         <Link to='/'>
-          <i className='fas fa-code'></i> DevConnector
+          <i className='fas fa-code'></i> DevConnect
         </Link>
       </h1>
       {!loading && (
